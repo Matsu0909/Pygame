2976438e8f4109e71359cdd7b2c0f7322b47c7eb
marcelo <marcelocp3@al.insper.edu.br>
2022-11-30 10:59:14 -0300

    a

--- Snake.py
+++ Snake.py
@@ -1,60 +1,35 @@
 import pygame
 import random
-from config import black,white,red,green,FPS,WIDTH,HEIGHT,APPLESIZE,SIZE,fontg,fontm,fontp,SIZEHEIGHT
-from funcoes import message, snake, score
-from assets import head_img,apple_img,tail_img
 
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((WIDTH,HEIGHT+SIZEHEIGHT))
+#cores
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+green = (0,255,0)
+
+FPS = 15
+
+WIDTH = 800
+HEIGHT = 600
+APPLESIZE = 10
+SIZE = 10
+gameDisplay = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('Snake')
-pygame.display.set_icon(apple_img)
 
 #clock
 clock = pygame.time.Clock()
-
-#Carrega sons do jogo
-chompsnd = pygame.mixer.Sound('assets/snd/chompsnd.mp3')
+font = pygame.font.SysFont(None,25)
 
 #funções
-def game_intro():
-    
-    intro = True
+def snake(SIZE,S_list):
+    for XeY in S_list:
+        pygame.draw.rect(gameDisplay,green,[XeY[0],XeY[1],SIZE,SIZE])
 
-    while intro:
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-            
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    intro = False
-                if event.key == pygame.K_SPACE:
-                    pygame.quit()
-                    quit()
-
-        gameDisplay.fill(white)
-        message("Bem-vindo à Python", 
-        green,gameDisplay,
-        -100, 
-        "large")
-        message("O objetivo do jogo é comer maçãs vermelhas e sobreviver",
-        black,gameDisplay,
-        -30)
-        message("Quanto mais maçãs você comer, maior você fica",
-        black,gameDisplay,
-        10)
-        message("Se você correr em si mesmo, ou nas bordas da tela, você morre",
-        black,gameDisplay,
-        50)
-        message("Pressione ENTER para iniciar ou ESPAÇO para sair",
-        black,gameDisplay,
-        180)
-
-        pygame.display.update()
-        clock.tick(5)
+def message(msg,color):
+    screen_text = font.render(msg,True,color)
+    gameDisplay.blit(screen_text, [WIDTH/2,HEIGHT/2])
 
 def gameLoop():
     gameExit = False
@@ -62,9 +37,8 @@ def gameLoop():
 
     lead_x = WIDTH/2
     lead_y = HEIGHT/2
-    lead_x_change = SIZE
+    lead_x_change = 0
     lead_y_change = 0
-    direction = 'leste'
 
     randAppleX = random.randrange(0,WIDTH-APPLESIZE,APPLESIZE)
     randAppleY = random.randrange(0,HEIGHT-APPLESIZE,APPLESIZE)
@@ -76,13 +50,10 @@ def gameLoop():
 
         while gameOver == True:
             gameDisplay.fill(black)
-            message("Aperte ENTER para jogar novamente ou Espaço para sair", red,gameDisplay)
+            message("Aperte ENTER para jogar novamente ou Espaço para sair", red)
             pygame.display.update()
 
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    gameExit = True
-                    gameOver = False
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE:
                         gameExit = True
@@ -97,19 +68,15 @@ def gameLoop():
                 gameExit = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    direction = 'oeste'
                     lead_x_change = -SIZE
                     lead_y_change = 0
                 elif event.key == pygame.K_RIGHT:
-                    direction = 'leste'
                     lead_x_change = SIZE
                     lead_y_change = 0
                 elif event.key == pygame.K_UP:
-                    direction = 'sul'
                     lead_y_change = -SIZE
                     lead_x_change = 0
                 elif event.key == pygame.K_DOWN:
-                    direction = 'norte'
                     lead_y_change = SIZE
                     lead_x_change = 0
 
@@ -119,11 +86,9 @@ def gameLoop():
 
         lead_x += lead_x_change
         lead_y += lead_y_change
-    
 
         gameDisplay.fill(white)
-        gameDisplay.blit(apple_img, (randAppleX,randAppleY))
-        #pygame.draw.rect(gameDisplay,red,[randAppleX,randAppleY,APPLESIZE,APPLESIZE])
+        pygame.draw.rect(gameDisplay,red,[randAppleX,randAppleY,APPLESIZE,APPLESIZE])
 
         #tamanho da cobra
         snakeHead = []
@@ -139,19 +104,14 @@ def gameLoop():
             if SEGMENT == snakeHead:
                 gameOver = True
 
-        snake(SIZE,snakeList,direction,gameDisplay)
+        snake(SIZE,snakeList)
 
-        score(snakeLength,gameDisplay)
+        pygame.display.update()  
 
-        pygame.display.update()
-
-        #comer
-        if lead_x >= randAppleX and lead_x <= randAppleX+APPLESIZE-SIZE:
-            if lead_y >= randAppleY and lead_y <= randAppleY+APPLESIZE-SIZE:
-                randAppleX = random.randrange(0,WIDTH-APPLESIZE,APPLESIZE)
-                randAppleY = random.randrange(0,HEIGHT-APPLESIZE,APPLESIZE)
-                snakeLength += 1
-                chompsnd.play()
+        if lead_x == randAppleX and lead_y == randAppleY:
+            randAppleX = random.randrange(0,WIDTH-SIZE,SIZE)
+            randAppleY = random.randrange(0,HEIGHT-SIZE,SIZE)
+            snakeLength += 1
 
         clock.tick(FPS) 
 
@@ -159,5 +119,4 @@ def gameLoop():
     pygame.quit()
     quit()
 
-game_intro()
 gameLoop()
