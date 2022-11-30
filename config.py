40de316b8194a37b37702f96906f2789d5bcb5c1
Matusita <matusitapedro@gmail.com>
2022-11-30 10:21:42 -0300

    rabo

--- config.py
+++ config.py
@@ -18,3 +18,7 @@ SIZE = 20
 fontp = pygame.font.SysFont('dejavusansmono',20)
 fontm = pygame.font.SysFont('dejavusansmono',30)
 fontg = pygame.font.SysFont('dejavusansmono',60)
+
+#gameDisplay.blit(head, (S_list[-1][0],S_list[-1][1]))
+ #   for XeY in S_list[:-1]:
+  #      pygame.draw.rect(gameDisplay,green,[XeY[0],XeY[1],SIZE,SIZE])
